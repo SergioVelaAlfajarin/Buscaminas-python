81f39a2348bbdf4e5ca7bf8ee332c54dc32df02e
SergioVelaAlfajarin <fdezvela.sergio.2002@gmail.com>
2022-10-11 11:22:04 +0200

    cambiado localizacion y agragada clase cell

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import settings
 import utils 
+from cell import Cell
 
 def main():
     root = Tk()
@@ -25,6 +26,21 @@ def main():
         height=utils.height_prct(80)
     ).place(x=0,y=utils.height_prct(20))
 
+    center_frame = Frame(
+        root,
+        bg="green",
+        width=utils.width_prct(80),
+        height=utils.height_prct(80)
+    ).place(x=utils.width_prct(20), y=utils.height_prct(20))
+    
+    btn1 = Button(
+        center_frame,
+        bg="white",
+        text="button1"
+    ).place(x=0,y=0)
+    
+    c1 = Cell()
+    
     # lanza la ventana
     root.mainloop()
 
